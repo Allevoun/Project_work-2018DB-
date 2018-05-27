29821b70ae4f004a025aea3944f021e4b8dd45f9
Levon Sargsyan <sn.levon@gmail.com>
2018-05-27 23:07:53 +0300

    Created DBaccess Created DBconnection Created SQLquery

---
+++ Project_work_DB/Repository/SQLquery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_work_DB.Repository
+{
+    public class SQLquery
+    {
+        public string Query_TargetPeople { get; set; }
+        public string Query_Product { get; set; }
+        public string Query_Managers { get; set; }
+        public string Query_Promotion { get; set; }
+        public string Query_Sales { get; set; }
+        public string Query_Market { get; set; }
+
+        public SQLquery()
+        {
+            Query_TargetPeople = "Select* FROM TARGET_PEOPLE";
+            Query_Sales = "Select* from SALES";
+            Query_Promotion = "Select* from PROMOTION";
+            Query_Product = "Select* from PRODUCT";
+            Query_Market = "Select* from MARKET";
+            Query_Managers = "Select* from MANAGERS";
+        }       
+    }
+}
